ba31147ad49f3dbe19bdcbef10919bf178d68261
NilesHachmeister <93550726+NilesHachmeister@users.noreply.github.com>
2022-03-02 10:03:49 -0700

    added the choices for what to build next or exit out

--- index.js
+++ index.js
@@ -38,75 +38,100 @@ function buildTeam() {
                 message: "Enter employee's office number",
                 name: 'officeNumber',
             },
-
-
-
         ])
         .then((data) => {
 
             let employee = new Manager(data.name, data.Id, data.email, data.officeNumber)
             teamMembers.push(employee)
-            console.log(teamMembers);
-            console.log(employee);
+       
+
+            promptForANewTeamMember()
         });
 
 
-
-    // promptForANewTeamMember()
 }
 
 
-// function promptForANewTeamMember() {
+function promptForANewTeamMember() {
 
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'list',
-//                 message: "Would you like to add another employee?",
-//                 name: 'addEmployee',
-//                 choices: ["Add an engineer", "Add an intern", "Finish building team"]
-//             },
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: "Would you like to add another employee?",
+                name: 'addEmployee',
+                choices: ["Add an engineer", "Add an intern", "Finish building team"]
+            },
+        ])
+        .then((data) => {
+            if (data.addEmployee === "Add an engineer") {
+                buildNewEngineer()
+            } else if (data.addEmployee === "Add an intern") {
+                buildNewIntern()
+            }else{
+                compileTeam()
+            }
 
-//         ])
-
-//         .then((data) => {
-//             console.log(data);
-
-//             count++
-//             console.log(count);
-
-//             if (data.addEmployee !== "Finish building team") {
-//                 buildNewTeamMember()
-//             } else {
-//                 return
-//             }
-//         });
-// }
+      
+        });
+}
 
 
 
 
-// function buildNewEngineer() {
-//     inquirer
-//         .prompt([
-//             {
-//                 type: 'input',
-//                 message: "Enter first user",
-//                 name: 'description',
-//             },
-//         ])
-//         .then((data) => {
+function buildNewEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter employee's name",
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: "Enter employee's ID",
+                name: 'Id',
+            },
+            {
+                type: 'input',
+                message: "Enter employee's email address",
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: "Enter employee's gitHub username",
+                name: 'gitHub',
+            },
+        ])
+        .then((data) => {
+
+
+            promptForANewTeamMember()
+        });
+}
+
+
+function buildNewIntern() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "Enter first user",
+                name: 'description',
+            },
+        ])
+        .then((data) => {
+
+
+            promptForANewTeamMember()
+        });
+}
 
 
 
-//         });
-//     promptForANewTeamMember()
-
-// }
-
-
-
-
+function compileTeam(){
+console.log(teamMembers);
+}
 
 
 
